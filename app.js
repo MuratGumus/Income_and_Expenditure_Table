0a392645bfde48cd854d6a0d45ce78066d63851f
gumusmuratgm@gmail.com <gumusmuratgm@gmail.com>
2023-06-11 22:07:09 +0300

    harcama body finish

--- app.js
+++ app.js
@@ -1,28 +1,74 @@
-//? Selectors
-const ekleBtn = document.getElementById('ekle-btn');
-const gelirInput = document.getElementById('gelir-input');
-const ekleFormu = document.getElementById('ekle-formu');;
+const ekleBtn = document.getElementById("ekle-btn");
+const gelirInput = document.getElementById("gelir-input");
+const ekleFormu = document.getElementById("ekle-formu");
+const gelirinizTd = document.getElementById("geliriniz");
+const harcamaFormu = document.getElementById("harcama-formu");
+const harcamaAlaniInput = document.getElementById("harcama-alani");
+const tarihInput = document.getElementById("tarih");
+const miktarInput = document.getElementById("miktar");
+const harcamaBody = document.getElementById("harcama-body");
 
-const gelirinizTd = document.getElementById('geliriniz');;
-//? Variables
-let gelirler = ""
+let gelirler = 0;
+let harcamaListesi = [];
 
+ekleFormu.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+    gelirler = gelirler + Number(gelirInput.value); 
+    localStorage.setItem("gelirler", gelirler);
+    ekleFormu.reset();
+    hesaplaVeGuncelle();
+});
 
+window.addEventListener("load", () => {
+    gelirler = Number(localStorage.getItem("gelirler"));
+    harcamaListesi = JSON.parse(localStorage.getItem("harcamalar")) || [];
+    harcamaListesi.forEach((harcama) => harcamayiDomaYaz(harcama));
+    console.log(harcamaListesi);
+    tarihInput.valueAsDate = new Date();
+    hesaplaVeGuncelle();
+});
 
-//?Events
+harcamaFormu.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+    const yeniHarcama = {
+        id: new Date().getTime(),
+        id: new Date().getTime(),
+        tarih: tarihInput.value,
+        alan: harcamaAlaniInput.value,
+        miktar: miktarInput.value,
+    };
 
-ekleFormu.addEventListener("submit", (e)=>{
-    e.preventDefault()
-    gelirler = Number(gelirler) + Number(gelirInput.value)
-    
-    ekleFormu.reset()
-
-    hesaplaVeGuncelle()
-})
-
-
-//? Functions
+    harcamaListesi.push(yeniHarcama);
+    localStorage.setItem("harcamalar", JSON.stringify(harcamaListesi));
+    harcamayiDomaYaz(yeniHarcama);
+    harcamaFormu.reset();
+    tarihInput.valueAsDate = new Date();
+    console.log(yeniHarcama);
+});
 
 const hesaplaVeGuncelle = () => {
-gelirinizTd.innerText = gelirler
-} 
+    gelirinizTd.innerText = gelirler;
+};
+
+const harcamayiDomaYaz = ({ id, miktar, tarih, alan }) => {
+    harcamaBody.innerHTML += `
+    <tr>
+        <td>${tarih}</td>
+        <td>${alan}</td>
+        <td>${miktar}</td>
+        <td><i id=${id} class="fa-solid fa-trash-can text-danger" type="button"></i></td>
+    </tr>
+    `;
+};
+
+harcamaBody.addEventListener("click", (e) => {
+    if (e.target.classList.contains("fa-trash-can")) {
+        e.target.parentElement.parentElement.remove();
+        const id = e.target.id;
+        console.log(id);
+        harcamaListesi = harcamaListesi.filter((harcama) => harcama.id != id);
+        localStorage.setItem("harcamalar", JSON.stringify(harcamaListesi));
+        hesaplaVeGuncelle();
+    }
+});
+
